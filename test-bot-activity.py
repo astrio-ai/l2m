2a21a6fb35173072abd079db535381618f017647
naingoolwin <naingoolwin.astrio@gmail.com>
2025-08-03 14:51:59 -0500

    test: Add file with formatting issues for bot testing

---
+++ test-bot-activity.py
@@ -0,0 +1,18 @@
+# Test file for Graphite bot activity
+# This file has some formatting issues that the bot might fix
+
+def test_function():
+    x=1+2  # Missing spaces around operators
+    y=3*4  # Missing spaces around operators
+    return x+y
+
+class TestClass:
+    def __init__(self):
+        self.value=0  # Missing spaces around assignment
+    
+    def get_value(self):
+        return self.value
+
+# Missing docstring
+def another_function():
+    pass 
