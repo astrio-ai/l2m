a025e97acef29a6dfe6696ced716a14396f25627
naingoolwin <naingoolwin.astrio@gmail.com>
2025-08-09 23:24:04 -0400

    Make API private: remove from tracking and add to .gitignore

--- api/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-FastAPI wrapper for Legacy2Modern CLI
-
-This package provides a REST API interface for the legacy2modern transpilation engine.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Legacy2Modern Team" 
